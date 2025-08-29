ffd5ad66b6894d9ac30239446074754242c1b477
Corentin Robac <71831663+Corentin-Robac@users.noreply.github.com>
2025-08-30 00:32:13 +0200

    feat: add cn utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,2 +1,6 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"; // ✅ Correct
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs));
+}
